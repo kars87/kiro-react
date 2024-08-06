770c7abacbe91ed0de0dead2339820b28fa70180
Kim Rosvoll <66441323+kars87@users.noreply.github.com>
2024-08-07 00:49:34 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+    <h2 className='h2'>Work in progress</h2>
+      <header className="App-header">
+        
+        <img src={logo} className="App-logo" alt="logo" />
+      
+      </header>
+     
+    </div>
+  );
+}
+
+export default App;
